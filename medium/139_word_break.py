265cd3cb25fd8bfb4cd0bc37f20af0c385aa6cdc
Shravan Kumar <shravan.nitwcse@gmail.com>
2023-08-04 20:13:06 +0530

    see if given string can be formed using words in a given word list

---
+++ medium/139_word_break.py
@@ -0,0 +1,27 @@
+"""
+https://leetcode.com/problems/word-break/description/
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+"""
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s)-1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i:i+len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]: break
+        
+        return dp[0]
