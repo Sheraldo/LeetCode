b56815da5011ca5f5fdbbf3b96bedac649491919
Shravan Kumar <shravan.nitwcse@gmail.com>
2023-08-11 09:50:53 +0530

    ways of making an amount from a coin denomination list with unlimited supply

---
+++ medium/518_coin_change_II.py
@@ -0,0 +1,33 @@
+"""
+https://leetcode.com/problems/coin-change-ii/description/
+
+Input: amount = 5, coins = [1,2,5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+
+Input: amount = 3, coins = [2]
+Output: 0
+Explanation: the amount of 3 cannot be made up just with coins of 2.
+
+Input: amount = 10, coins = [10]
+Output: 1
+"""
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[-1 for _ in range(len(coins))] for __ in range(amount + 1)]
+
+        def ways(coins, i, rem_amnt, dp):
+            if i < 0: return int(rem_amnt == 0)
+            if dp[rem_amnt][i] != -1: return dp[rem_amnt][i]
+            cnt = ways(coins,i-1, rem_amnt, dp)
+            if rem_amnt >= coins[i]:
+                cnt += ways(coins, i, rem_amnt - coins[i], dp)
+            dp[rem_amnt][i] = cnt
+            return dp[rem_amnt][i]
+
+        return ways(coins, len(coins)-1, amount, dp)
+
