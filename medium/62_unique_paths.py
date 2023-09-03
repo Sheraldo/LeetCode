90b8826cf537b5612f25e83d0088ee1cd7efc263
Shravan Kumar <shravan.nitwcse@gmail.com>
2023-09-03 12:43:24 +0530

    [62][medium] unique paths to each bottom right from top left

---
+++ medium/62_unique_paths.py
@@ -0,0 +1,30 @@
+"""
+https://leetcode.com/problems/unique-paths/description/?envType=daily-question&envId=2023-09-03
+
+Input: m = 3, n = 7
+Output: 28
+
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+"""
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * n for _ in range(m)]
+        dp[0][0] = 1
+
+        for i in range(n):
+            dp[0][i] = 1
+
+        for i in range(m):
+            dp[i][0] = 1
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        
+        return dp[m-1][n-1]
+
