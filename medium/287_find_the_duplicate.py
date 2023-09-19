3cdff2a443aa471f5a268f083451923f599863f8
Shravan Kumar <shravan.nitwcse@gmail.com>
2023-09-19 11:44:48 +0530

    [287][medium] find the duplicate number in array

---
+++ medium/287_find_the_duplicate.py
@@ -0,0 +1,39 @@
+"""
+https://leetcode.com/problems/find-the-duplicate-number/?envType=daily-question&envId=2023-09-19
+
+You must solve the problem without modifying the array nums and uses only constant extra space.
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+
+Constraints:
+
+    1 <= n <= 105
+    nums.length == n + 1
+    1 <= nums[i] <= n
+    All the integers in nums appear only once except for precisely one integer which appears two or more times.
+"""
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0,0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        
+        slow2 = 0
+        while True:
+            slow2 = nums[slow2]
+            slow = nums[slow]
+            if slow == slow2:
+                return slow
+
