5eec3032d9c3099ebf4a4a4b6074c6c99c721c98
Shravan Kumar <shravan.nitwcse@gmail.com>
2023-09-22 09:14:53 +0530

    [392][easy] return true if s is a subsequence of t

---
+++ easy/392_is_subsequence.py
@@ -0,0 +1,29 @@
+"""
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+
+ 
+
+Constraints:
+
+    0 <= s.length <= 100
+    0 <= t.length <= 104
+    s and t consist only of lowercase English letters.
+
+"""
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+        while i<len(s) and j<len(t):
+            if s[i]==t[j]:
+                i += 1
+            j += 1
+        return i==len(s)
+
